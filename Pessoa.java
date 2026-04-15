cb09f3d9899fafd8b329547b92d254132098883b
vitor7883802-boop <vitor.7883802@aluno.mg.gov.br>
2026-04-15 10:32:19 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+String nome;
+int idade;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
